9706ae796e503226f820c8714ce1eb960147198b
Shanaz King <sking1128@YewYew1128.localdomain>
2024-05-15 15:20:34 -0400

    making changes to db config

--- db.js
+++ db.js
@@ -1,16 +1,19 @@
+
+
+
 //Another workaround
 "use strict";
 /** Database setup for jobly. */
 const { Client } = require("pg");
 const { getDatabaseUri } = require("./config");
 
-let DB_URI;
+let DB_URI = "postgresql:///react_jobly_solution";
 
-if (process.env.NODE_ENV === "test") {
-  DB_URI = "postgresql:///react_jobly_solution_test";
-} else {
-  DB_URI = "postgresql:///react_jobly_solution";
-}
+// if (process.env.NODE_ENV === "test") {
+//   DB_URI = "postgresql:///react_jobly_solution_test";
+// } else {
+//   DB_URI = "postgresql:///react_jobly_solution";
+// }
 
 let db = new Client({
   connectionString: DB_URI
